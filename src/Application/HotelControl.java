1f190f12e1bcc5affbfc37d4e0e70f6d1db0f65b
PawelJuszczak <juszczakpawel@gmail.com>
2017-11-29 13:35:34 +0100

    deletet string levelstatus from Room class

--- src/Application/HotelControl.java
+++ src/Application/HotelControl.java
@@ -1,5 +1,15 @@
 package Application;
 
 public class HotelControl {
-
+	
+	
+	
+	private enum Option{
+		EXIT(0, "Exit program."),
+		CREATE_NEW_HOTEL(1, "Create new Hotel structure. "),
+		ADMINISTRATE_HOTEL(2, "Administarate already created hotel")
+		
+		
+		
+	}
 }
